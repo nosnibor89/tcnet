869fab827f449a5854fb93abc5aa67da8bc31702
Robinson <nosnibor1989@gmail.com>
2018-02-15 16:37:06 -0300

    Initial Setup

---
+++ TCNet/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var deployCounts = 1;
+            return View("Index", deployCounts);
+        }
+    }
+}
